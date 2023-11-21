16a824b0d3ce3f36ff2afced26365c64cb058457
r1gz <r1gzhvhkid@gmail.com>
2023-11-21 13:59:25 +0300

    fixes

--- tasks/task_1/bodrov_d_max_row_values/max_row_values.h
+++ tasks/task_1/bodrov_d_max_row_values/max_row_values.h
@@ -1,7 +1,6 @@
 // Copyright 2023 Bodrov Daniil
-#ifndef MAX_ROW_VALUES_H
-#define MAX_ROW_VALUES_H
-
+#ifndef TASKS_TASK_1_BODROV_D_MAX_ROW_VALUES_MAX_ROW_VALUES_H_
+#define TASKS_TASK_1_BODROV_D_MAX_ROW_VALUES_MAX_ROW_VALUES_H_
 #include <mpi.h>
 #include <vector>
 
@@ -9,4 +8,4 @@ std::vector<int> FindMaxRowValues(const std::vector<int>& matrix, int n);
 std::vector<int> FindMaxRowValuesPar(const std::vector<int>& matrix, int n);
 
 
-#endif  // MAX_ROW_VALUES_H
+#endif  // TASKS_TASK_1_BODROV_D_MAX_ROW_VALUES_MAX_ROW_VALUES_H_
